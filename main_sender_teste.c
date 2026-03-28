e959569354c7ac5339a467f5626ec1de66c781b0
Eduardo Reichert <eduardobasanezreichert@gmail.com>
2026-03-28 18:10:32 -0300

    Modify main to use FIFO for image processing
    Refactor main function to send image header and data through a named pipe.

--- main_sender_teste.c
+++ main_sender_teste.c
@@ -1,78 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct PGM{
-  int w, h, maxv;      //maxv = 255
-  unsigned char* data; //w*h bytes
-}PGM;
-
-int read_PGM(const char* path, PGM* img);
-int write_PGM(const char* path, const PGM* img);
+#include "trabalhOS.h"
 
 int main(int argc, char* argv[]){
   PGM imagem;
-  //salva a imagem na struct PGM, em imagem.data
+  Header cabecalho;
+
+  //um caminho que não muda
+  const char* path = FIFO_PATH;
+  mkfifo(path, 0666); //cria a named pipe
+
+  //lê e salva a imagem na struct PGM, em imagem.data
   read_PGM("img_mux.pgm", &imagem);
 
-  //TORNA NEGATIVA A IMAGEM ORIGINAL
-  for(register int i=0; i < imagem.h * imagem.w; i++){
-    imagem.data[i] = 255 - imagem.data[i];
-  }
+  //prepara o header para enviar para o worker
+  cabecalho.w = imagem.w;
+  cabecalho.h = imagem.h;
+  cabecalho.maxv = imagem.maxv;
+  cabecalho.mode = NEGATIVO;
+  cabecalho.t1 = 0;
+  cabecalho.t2 = 0;
 
-  write_PGM("NEG_img_mux.pgm", &imagem);
+  //abre a named pipe para que o worker receba a imagem e o cabeçalho
+  printf("Esperando pelo Worker...");
+  int fd;
+  fd = open(path, O_WRONLY);
+  printf("Worker conectado. Enviando imagem e cabecalho");
 
-  return 0;
-}
+  //envia a imagem e o cabecalho para o main_worker.c
+  write(fd, &cabecalho, sizeof(Header));
+  write(fd, imagem.data, imagem.w * imagem.h);
 
-int read_PGM(const char* path, PGM* img){
-  FILE* file = fopen(path, "rb");
-  if(!file){
-    perror("Erro ao abrir o arquivo.");
-    return -1;
-  }
+  printf("Dados enviados.");
 
-  char magic[4]; //DEVE ACHAR "P5" NA PRIMEIRA LINHA SEMPRE
-  /*Aqui o numero mágico é P5 que está na primeira fila mas a linha é P5\n ai precisamos deixar espaço para 3 +1
-  o +1 é por que o fgets coloca '\0' no final do vetor, entao são 3 espaços para P5\n +1 para o \0
-  por isso fica magic[4] */
-  if(!fgets(magic, sizeof(magic), file)){
-    printf("Arquivo vazio.\n");
-    fclose(file);
-    return -1;
-  }else{
-    if(magic[0] != 'P' || magic[1] != '5'){ //verifica se tem o P5 na primeira linha
-      printf("Não é um arquivo PGM.");
-      fclose(file);
-      return -1;
-    }
-  }
+  close(fd);
+  free(imagem.data);
 
-  //retira os comentários
-  int ch;
-  while((ch = fgetc(file)) == '#'){
-    while(fgetc(file) != '\n');
-  }
-  ungetc(ch, file);
-
-  fscanf(file, "%d %d %d", &img->w, &img->h, &img->maxv);
-
-  fgetc(file);
-
-  //Aloca memoria para o data e então lê a imagem
-  img->data = (unsigned char*)malloc(img->w * img->h * sizeof(unsigned char));
-  fread(img->data, 1, img->w * img->h, file);
-
-  fclose(file);
-  return 0;
-}
-
-int write_PGM(const char* path, const PGM* img){
-  FILE* file = fopen(path, "wb");
-  fprintf(file, "P5\n%d %d\n %d\n", img->w, img->h, img->maxv);
-
-  fwrite(img->data, 1, img->w * img->h, file);
-
-  fclose(file);
   return 0;
 }
