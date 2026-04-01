d211313efb508dcddfe1ca4654257305d9029270
RafaelRibas2 <rafaelburattoribas@gmail.com>
2026-04-01 12:01:30 +0000

    teste1

--- trabalhOS.h
+++ trabalhOS.h
@@ -68,7 +68,10 @@ int read_PGM(const char* path, PGM* img){
   img->data = (unsigned char*)malloc(img->w * img->h * sizeof(unsigned char));
   fread(img->data, 1, img->w * img->h, file);
 
-  //teste
+  while(1){
+    int i = 1;
+    i +=1;
+  }
   fclose(file);
   return 0;
 }
