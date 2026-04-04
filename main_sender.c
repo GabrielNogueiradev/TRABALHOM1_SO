de5877be9265f4ebaf91215732ccf1ed3f28aa9e
Eduardo Reichert <eduardobasanezreichert@gmail.com>
2026-04-04 10:41:18 -0300

    Update image file name and improve data sending

--- main_sender.c
+++ main_sender.c
@@ -9,7 +9,7 @@ int main(int argc, char* argv[]){
   mkfifo(path, 0666); //cria a named pipe
 
   //lê e salva a imagem na struct PGM, em imagem.data
-  read_PGM("img_mux.pgm", &imagem);
+  read_PGM("fisica.pgm", &imagem);
 
   //prepara o header para enviar para o worker
   cabecalho.w = imagem.w;
@@ -20,16 +20,22 @@ int main(int argc, char* argv[]){
   cabecalho.t2 = 0;
 
   //abre a named pipe para que o worker receba a imagem e o cabeçalho
-  printf("Esperando pelo Worker...");
+  printf("Esperando pelo Worker...\n");
   int fd;
   fd = open(path, O_WRONLY);
-  printf("Worker conectado. Enviando imagem e cabecalho");
+  printf("Worker conectado. Enviando imagem e cabecalho\n");
 
   //envia a imagem e o cabecalho para o main_worker.c
   write(fd, &cabecalho, sizeof(Header));
-  write(fd, imagem.data, imagem.w * imagem.h);
 
-  printf("Dados enviados.");
+  size_t tamanho_esperado = imagem.w * imagem.h;
+  size_t tamanho_enviado = 0;
+  while(tamanho_enviado < tamanho_esperado){
+    size_t n = write(fd, imagem.data + tamanho_enviado, tamanho_esperado - tamanho_enviado);
+    tamanho_enviado += n;
+  }
+
+  printf("Dados enviados.\n");
 
   close(fd);
   free(imagem.data);
